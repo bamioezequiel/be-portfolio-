da1c2c88cedbc74f6fd43a0ae522b86fa6b16e5c
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-08-16 00:04:32 -0300

    added icons

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import { TbWorld } from 'react-icons/tb';
 // import { MdLightMode, MdModeNight } from 'react-icons/md';
+import { AiOutlineGlobal, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineFileText, AiOutlineContacts } from 'react-icons/ai';
 import { NavLink } from 'react-router-dom';
 import s from './Nav.module.css';
 
@@ -9,12 +9,18 @@ export default function Nav() {
         <div className={s.nav_container}>
             <NavLink to='/' className={s.nav_logo}>BE</NavLink>
             <div className={s.nav_menu}>
-                <NavLink to='/' className={s.nav_item}>Home</NavLink>
-                <NavLink to='/' className={s.nav_item}>Projects</NavLink>
-                <NavLink to='/' className={s.nav_item}>Resume</NavLink>
-                <NavLink to='/' className={s.nav_item}>Contact Me</NavLink>
-                {/* <NavLink to='/' className={s.nav_item_ico}><TbWorld /></NavLink> */}
-                {/* <NavLink to='/' className={s.nav_item_ico}><MdModeNight /></NavLink> */}
+                <NavLink to='/' className={s.nav_item}>
+                    <AiOutlineHome className={s.nav_item_icon} /> Home
+                </NavLink>
+                <NavLink to='/' className={s.nav_item}>
+                    <AiOutlineFundProjectionScreen className={s.nav_item_icon} /> Projects
+                </NavLink>
+                <NavLink to='/' className={s.nav_item}>
+                    <AiOutlineFileText className={s.nav_item_icon} /> Resume
+                </NavLink>
+                <NavLink to='/' className={s.nav_item}>
+                    <AiOutlineContacts className={s.nav_item_icon} />Contact Me
+                </NavLink>
             </div>           
         </div>
     )
